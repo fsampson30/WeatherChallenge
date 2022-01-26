99d0ff58d85168b2c680a70ab7c18e37916e4b0b
fsampson30@ <fsampson30@gmail.com>
2022-01-26 08:16:33 -0300

    Removed unnecessary imports.

--- app/src/main/java/com/sampson/weatherchallenge/CityDetailsActivity.kt
+++ app/src/main/java/com/sampson/weatherchallenge/CityDetailsActivity.kt
@@ -1,19 +1,11 @@
 package com.sampson.weatherchallenge
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.gson.GsonBuilder
 import com.sampson.weatherchallenge.model.City
 import retrofit2.Call
